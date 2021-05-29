da4b06a78eeaf4c7446abd11597143cf8b53e30a
prajwal.poudel <prajwal.poudel@ebpearls.com>
2021-05-29 09:56:41 +0545

    ~hotfixes assets

--- src/resources/views/includes/script-footer.blade.php
+++ src/resources/views/includes/script-footer.blade.php
@@ -1,29 +1,29 @@
-<script src="{{ asset('vendor/metronics') }}/assets/js/jquery.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/popper.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/js.cookie.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/moment.min.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/tooltip.min.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/perfect-scrollbar.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/sticky.min.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/wNumb.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/jquery.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/popper.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/js.cookie.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/moment.min.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/tooltip.min.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/perfect-scrollbar.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/sticky.min.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/wNumb.js" type="text/javascript"></script>
 
-<!--end:: Global Mandatory Vendors -->
+<!--end:: Global Mandatory s -->
 
-<!--begin:: Global Optional Vendors -->
-<script src="{{ asset('vendor/metronics') }}/assets/js/jquery.blockUI.js" type="text/javascript"></script>
-<!--end:: Global Optional Vendors -->
+<!--begin:: Global Optional s -->
+<script src="{{ asset('metronics') }}/js/jquery.blockUI.js" type="text/javascript"></script>
+<!--end:: Global Optional s -->
 
 
 <!--begin::Global Theme Bundle(used by all pages) -->
-<script src="{{ asset('vendor/metronics') }}/assets/js/scripts.bundle.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/scripts.bundle.js" type="text/javascript"></script>
 
 <!--end::Global Theme Bundle -->
 
 
 {{--Additional Scripts--}}
-<script src="{{ asset('vendor/metronics') }}/assets/js/datatables.bundle.js" type="text/javascript"></script>
-<script src="{{ asset('vendor/metronics') }}/assets/js/select2.full.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/datatables.bundle.js" type="text/javascript"></script>
+<script src="{{ asset('metronics') }}/js/select2.full.js" type="text/javascript"></script>
 
 {{--Additional Scripts--}}
 
